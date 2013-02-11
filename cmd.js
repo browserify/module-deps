62388e9ad34de46928c00ab3d3cfe481d30c1db0
James Halliday <mail@substack.net>
2013-02-11 06:31:30 -0800

    bin script

---
+++ cmd.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+var parser = require('./');
+var JSONStream = require('JSONStream');
+
+var stringify = JSONStream.stringify();
+stringify.pipe(process.stdout);
+
+parser(process.argv.slice(2)).pipe(stringify);
