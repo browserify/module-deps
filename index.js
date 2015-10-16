c4b3527b7391eeb1f086c0677be44f6649734970
Kensuke Matsuzaki <matsuzaki@kitec.co.jp>
2015-10-16 13:14:10 +0900

    guard walk() against not having a cb

--- index.js
+++ index.js
@@ -324,7 +324,7 @@ Deps.prototype.walk = function (id, parent, cb) {
         if (opts.postFilter && !opts.postFilter(id, file, pkg)) {
             if (--self.pending === 0) self.push(null);
             if (input) --self.inputPending;
-            return cb(null, undefined);
+            return cb && cb(null, undefined);
         }
         if (err && rec.source) {
             file = rec.file;
