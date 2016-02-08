3958c48962f54cea25e0b98b4fd18fd4d4793b19
shinout <shinout310@gmail.com>
2016-02-08 18:28:55 +0900

    make packageDir options optional

--- index.js
+++ index.js
@@ -255,7 +255,8 @@ Deps.prototype.getTransforms = function (file, pkg, opts) {
             trOpts = tr[1];
             tr = tr[0];
         }
-        trOpts.packageDir = pkg.__dirname;
+        if (!trOpts.packageDir)
+            trOpts.packageDir = pkg.__dirname;
         if (typeof tr === 'function') {
             var t = tr(file, trOpts);
             self.emit('transform', t, file);
